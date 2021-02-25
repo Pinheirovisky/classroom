b3e5b56a07fd9254fa149c4d06cfd37c76e365cb
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-02-25 17:57:48 -0300

    feat: added project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import { main } from './helpers/index.js'
+import { parseDay } from './helpers/strings.js'
+
+const App: React.FC = () => {
+
+  const [inputValue, setInputValue] = useState("0")
+
+  const [result, setResult] = useState(null)
+
+  const handleSubmit = (): void => {
+
+    const value = parseInt(inputValue, 10)
+
+    const response = main(value)
+
+    return setResult(response)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h1>WTP</h1>
+        <label htmlFor="students">
+          Quantidade de alunos
+        <input type="string" name="students" id="students" value={inputValue} onChange={e => setInputValue(e.target.value)} />
+        </label>
+        <button type="submit" onClick={handleSubmit}>Buscar</button>
+
+        {result && (
+          <>
+            {console.log(result)}
+            <h3>Número <span>MÁXIMO</span> de professores: {result.max}</h3>
+            <h3>Número <span>MÍNIMO</span> de professores: {result.min}</h3>
+            <table>
+              <tr>
+                <th>Dia da semana</th>
+                <th>Horário</th>
+                <th>Salas simultâneas</th>
+              </tr>
+              {result.schedules.map((line, id) => {
+                return (
+                  <tr key={id}>
+                    <td>{`${parseDay(Number((line.schedule.split('-'))[0]))}`}</td>
+                    <td>{`${line.schedule.split('-')[1]}:00`}</td>
+                    <td>{line.rooms_concurrently}</td>
+                  </tr>
+                )
+              })}
+            </table>
+          </>
+        )}
+      </div>
     </div>
   );
 }
