71d2d0cb6b8d58db62bf6354b8d4abc99ab7496e
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-03-03 09:58:33 -0300

    feat: change routes

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
         <Route path="/classroom">
           <Classroom />
         </Route>
-        <Route path="/search">
+        <Route path="/">
           <Search />
         </Route>
       </Switch>
